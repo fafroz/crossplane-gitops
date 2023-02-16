4d5e95ba0d0505c80422ece7a0f8b6de0aacadb3
Fahad Afroz <fafroz@dxc.com>
2023-02-16 14:00:12 +0530

    Update gcp_config.sh

--- dev/scripts/gcp_config.sh
+++ dev/scripts/gcp_config.sh
@@ -1,13 +1,13 @@
 
 #!/bin/bash
 
-sh ../../dev/scripts/env_vars.sh
+sh dev/scripts/env_vars.sh
 
 # Activate the service account
 gcloud auth activate-service-account $GCP_SERVICE_ACCOUNT_NAME --key-file=$GCP_SERVICE_ACCOUNT_KEYFILE
 
 # Set GCP project
-gcloud config set project $GCP_PROJECT_ID
+echo N | gcloud config set project $GCP_PROJECT_ID 
 
 # Connect to cluster (also sets current context)
-gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $GKE_CLUSTER_ZONE --project $GCP_PROJECT_ID
+gcloud container clusters get-credentials $GKE_EXISTING_CLUSTER_NAME --zone $GKE_CLUSTER_ZONE --project $GCP_PROJECT_ID
