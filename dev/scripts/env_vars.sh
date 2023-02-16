d273f36d6d73a2dfe1d0bd7da07c7424ee36da73
Fahad Afroz <fafroz@dxc.com>
2023-02-16 09:34:11 +0530

    Update env_vars.sh

--- dev/scripts/env_vars.sh
+++ dev/scripts/env_vars.sh
@@ -5,7 +5,7 @@
 
 export GCP_PROJECT_ID="gitops-vittal"
 export GCP_SERVICE_ACCOUNT_NAME="gitops-cloudbuild-gke@gitops-vittal.iam.gserviceaccount.com"
-export GCP_SERVICE_ACCOUNT_KEYFILE="/home/fafroz/crossplane-gke-sqldb/GCP_SERVICE_ACCOUNT_KEYFILE"
+export GCP_SERVICE_ACCOUNT_KEYFILE="/crossplane-gitops/GCP_SERVICE_ACCOUNT_KEYFILE"
 export BASE64ENCODED_GCP_PROVIDER_CREDS=$(base64 GCP_SERVICE_ACCOUNT_KEYFILE | tr -d "\n") # base64 encode the GCP credentials
 
 export GKE_EXISTING_CLUSTER_NAME="cluster-f"
@@ -20,3 +20,6 @@ export GKE_NODEPOOL="gke-crossplane-np-f"
 # Set environment vars for Crossplane installation
 export CROSSPLANE_VERSION="1.2.0"
 export CROSSPLANE_NS="crossplane-system"
+
+
+# Set Cloud SQl Database (WIP)
