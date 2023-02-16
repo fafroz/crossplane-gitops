273a23607327d4b89557d5dfcd3aed937d3f749f
Fahad Afroz <fafroz@dxc.com>
2023-02-16 09:11:32 +0530

    Update install_crossplane.sh

--- dev/scripts/install_crossplane.sh
+++ dev/scripts/install_crossplane.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+
+. ./scripts/env_vars.sh
+
+# Install Crossplane
+kubectl create namespace $CROSSPLANE_NS
+helm repo add crossplane-stable https://charts.crossplane.io/stable
+helm repo update
+helm install crossplane --namespace $CROSSPLANE_NS crossplane-stable/crossplane --version $CROSSPLANE_VERSION
