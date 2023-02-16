2c5679c20d4bf3ddf1dd40196ebeb14f27769179
Fahad Afroz <fafroz@dxc.com>
2023-02-16 16:50:22 +0530

    Update configure_gcp_provider.sh

--- dev/scripts/configure_gcp_provider.sh
+++ dev/scripts/configure_gcp_provider.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-sh dev/scripts/env_vars.sh
+#sh dev/scripts/env_vars.sh
+. ./dev/scripts/env_vars.sh
 
 # Install the GCP Provider on Crossplane
-kubectl apply -f crossplane/provider-gcp.yml
+kubectl apply -f dev/crossplane/provider-gcp.yml
 sleep 10
 
 
 # Use envsubst to replace env var values in the provider-config YAML template file
 envsubst \
-    < crossplane/provider-config-gcp.template.yml \
-    > crossplane/provider-config-gcp.yml
+    < dev/crossplane/provider-config-gcp.template.yml \
+    > dev/crossplane/provider-config-gcp.yml
 
 # Configure the GCP Provider
-kubectl apply -f crossplane/provider-config-gcp.yml
+kubectl apply -f dev/crossplane/provider-config-gcp.yml
