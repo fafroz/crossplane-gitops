e1737cea0bed4f26feaf09b4072258d0eb3347b9
Fahad Afroz <fafroz@dxc.com>
2023-02-16 09:10:02 +0530

    Update create_gke_cluster.sh

--- dev/scripts/create_gke_cluster.sh
+++ dev/scripts/create_gke_cluster.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+. ./scripts/env_vars.sh
+
+# Use envsubst to replace env var values in the cluster-template YAML template file
+envsubst \
+    < crossplane/cluster-template.yml \
+    > crossplane/cluster.yml
+
+# Configure the cluster yml and install the GKE cluster
+kubectl apply -f crossplane/cluster.yml
