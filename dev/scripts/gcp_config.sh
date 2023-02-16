4ff7f6d1aa44d4271159279b3314f7f75dd8e913
Fahad Afroz <fafroz@dxc.com>
2023-02-16 16:51:49 +0530

    Update gcp_config.sh

--- dev/scripts/gcp_config.sh
+++ dev/scripts/gcp_config.sh
@@ -7,6 +7,7 @@
 # Activate the service account
 gcloud auth activate-service-account $GCP_SERVICE_ACCOUNT_NAME --key-file=$GCP_SERVICE_ACCOUNT_KEYFILE
 
+sh dev/scripts/env_vars.sh
 # Set GCP project
 echo N | gcloud config set project $GCP_PROJECT_ID 
 
