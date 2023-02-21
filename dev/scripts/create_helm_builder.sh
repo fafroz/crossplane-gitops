0e9093a7b89e25af48cf3f9b68c4826a7e9bc5e7
Fahad Afroz <fafroz@dxc.com>
2023-02-21 11:00:32 +0530

    Update create_helm_builder.sh

--- dev/scripts/create_helm_builder.sh
+++ dev/scripts/create_helm_builder.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/GoogleCloudPlatform/cloud-builders-community.git
 cd cloud-builders-community/helm
 GOOGLE_CLOUD_PROJECT=gitops-vittal
-GCR_HELM=us-central1-docker.pkg.dev/gitops-vittal/helm/helm-image
+GCR_HELM=us-central1-docker.pkg.dev/gitops-vittal/helm:TAG
 docker build -t $GCR_HELM
 docker push $GCR_HELM
-
