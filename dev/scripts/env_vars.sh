e79b164ddafbbde63a2f1c853475d20bee3665ae
Fahad Afroz <fafroz@dxc.com>
2023-02-22 14:56:14 +0530

    Update env_vars.sh

--- dev/scripts/env_vars.sh
+++ dev/scripts/env_vars.sh
@@ -1,8 +1,34 @@
+# #!/bin/bash
+
+# # Set environement variables for Google Cloud
+
+# # chmod +x ../../gcp_service_account_keyfile
+
+# export GCP_PROJECT_ID="gitops-vittal"
+# echo $GCP_PROJECT_ID
+# export GCP_SERVICE_ACCOUNT_NAME="gitops-cloudbuild-gke@gitops-vittal.iam.gserviceaccount.com"
+# export GCP_SERVICE_ACCOUNT_KEYFILE="gcp_service_account_keyfile"
+# export BASE64ENCODED_GCP_PROVIDER_CREDS=$(base64 $GCP_SERVICE_ACCOUNT_KEYFILE | tr -d "\n") # base64 encode the GCP credentials
+
+# export GKE_EXISTING_CLUSTER_NAME="cluster-fc"
+# export GKE_CLUSTER_ZONE="us-central1-c"
+
+# export GKE_CLUSTER_NAME="gke-crossplane-cluster-fc"
+# export GKE_NETWORK="gitops-vpc"
+# export GKE_SUBNETWORK="gitops-subnet1"
+
+# export GKE_NODEPOOL="gke-crossplane-np-f"
+
+# # Set environment vars for Crossplane installation
+# export CROSSPLANE_VERSION="1.2.0"
+# export CROSSPLANE_NS="crossplane-system"
+
+
+# # Set Cloud SQl Database (WIP)..
+
 #!/bin/bash
 
 # Set environement variables for Google Cloud
-
-# chmod +x ../../gcp_service_account_keyfile
 
 export GCP_PROJECT_ID="gitops-vittal"
 echo $GCP_PROJECT_ID
@@ -10,18 +36,22 @@ export GCP_SERVICE_ACCOUNT_NAME="gitops-cloudbuild-gke@gitops-vittal.iam.gservic
 export GCP_SERVICE_ACCOUNT_KEYFILE="gcp_service_account_keyfile"
 export BASE64ENCODED_GCP_PROVIDER_CREDS=$(base64 $GCP_SERVICE_ACCOUNT_KEYFILE | tr -d "\n") # base64 encode the GCP credentials
 
-export GKE_EXISTING_CLUSTER_NAME="cluster-fc"
-export GKE_CLUSTER_ZONE="us-central1-c"
+#Setting variables for MGCP-Host-crossplane-k8SCluster - CrossplaneHost_env_vars
+export GKE_HOST_CLUSTER_NAME="mgcp-host-crossplane-k8scluster"
+export GKE_HOST_CLUSTER_MAX_NODE="1"
+export GKE_HOST_CLUSTER_ZONE="us-central1-c"
+export GKE_HOST_CLUSTER_NETWORK="gitops-vpc"
+export GKE_HOST_CLUSTER_SUBNETWORK="gitops-subnet1"
 
-export GKE_CLUSTER_NAME="gke-crossplane-cluster-fc"
-export GKE_NETWORK="gitops-vpc"
-export GKE_SUBNETWORK="gitops-subnet1"
-
-export GKE_NODEPOOL="gke-crossplane-np-f"
-
-# Set environment vars for Crossplane installation
+# Set environment vars for Crossplane installation - CrossplaneHost_env_vars
 export CROSSPLANE_VERSION="1.2.0"
 export CROSSPLANE_NS="crossplane-system"
 
+
+#customer_env_var
+export GKE_CLUSTER_NAME="gke-k8scluster"
+export GKE_NETWORK="gitops-vpc"
+export GKE_SUBNETWORK="gitops-subnet1"
+export GKE_NODEPOOL="gke-crossplane-np-f"
 
 # Set Cloud SQl Database (WIP)..
