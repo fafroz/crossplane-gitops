d009a2014db6c077078c1ac3e8b1a08b14a5cbf0
Fahad Afroz <fafroz@dxc.com>
2023-02-23 12:39:27 +0530

    Update env_vars.sh

--- dev/scripts/env_vars.sh
+++ dev/scripts/env_vars.sh
@@ -1,31 +1,3 @@
-# #!/bin/bash
-
-# # Set environement variables for Google Cloud
-
-# # chmod +x ../../gcp_service_account_keyfile
-
-# export GCP_PROJECT_ID="gitops-vittal"
-# echo $GCP_PROJECT_ID
-# export GCP_SERVICE_ACCOUNT_NAME="gitops-cloudbuild-gke@gitops-vittal.iam.gserviceaccount.com"
-# export GCP_SERVICE_ACCOUNT_KEYFILE="gcp_service_account_keyfile"
-# export BASE64ENCODED_GCP_PROVIDER_CREDS=$(base64 $GCP_SERVICE_ACCOUNT_KEYFILE | tr -d "\n") # base64 encode the GCP credentials
-
-# export GKE_EXISTING_CLUSTER_NAME="cluster-fc"
-# export GKE_CLUSTER_ZONE="us-central1-c"
-
-# export GKE_CLUSTER_NAME="gke-crossplane-cluster-fc"
-# export GKE_NETWORK="gitops-vpc"
-# export GKE_SUBNETWORK="gitops-subnet1"
-
-# export GKE_NODEPOOL="gke-crossplane-np-f"
-
-# # Set environment vars for Crossplane installation
-# export CROSSPLANE_VERSION="1.2.0"
-# export CROSSPLANE_NS="crossplane-system"
-
-
-# # Set Cloud SQl Database (WIP)..
-
 #!/bin/bash
 
 # Set environement variables for Google Cloud
